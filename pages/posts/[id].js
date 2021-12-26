df0fc367ac71822a3ac066384bef9d3a5f98530c
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-12-26 18:12:15 +0530

    added [id].js for blogs

---
+++ pages/posts/[id].js
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import Date from '../../components/date'
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export async function getStaticPaths() {
+  // Return a list of possible value for id
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1>{postData.title}</h1>
+        <div>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
