221adc11a6898fb77711119bc037229ac840a9e7
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-12-26 17:28:51 +0530

    source error correction

--- components/navbar.js
+++ components/navbar.js
@@ -79,7 +79,7 @@ const Navbar = props => {
           </LinkItem>
           <LinkItem
             target="_blank"
-            href="https://github.com/craftzdog/craftzdog-homepage"
+            href="https://github.com/rosekamallove/rosekamallove.github.io"
             path={path}
             display="inline-flex"
             alignItems="center"
