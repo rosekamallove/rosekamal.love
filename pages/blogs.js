6a9d9b49f9ba98cc30ac480d3589e29f4d696dd8
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-12-26 18:19:15 +0530

    content of posts done

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,13 +1,39 @@
 import { Container, Heading } from '@chakra-ui/react'
+import Link from 'next/link'
+import Date from '../components/date'
 import Layout from '../components/layouts/article'
+import { getSortedPostsData } from '../lib/posts'
 
-const Page = () => {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+const Page = ({ allPostsData }) => {
   return (
     <Layout>
       <Container maxW="container.lg">
         <Heading as="h2" variant="page-title">
           Blogs
         </Heading>
+        {/* Blogs Section */}
+        <ul>
+          {allPostsData.map(({ id, date, title }) => (
+            <li key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </Container>
     </Layout>
   )
