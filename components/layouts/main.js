051dbb5741b2616ea94ffcc162dcc979b73b8ece
Rose Kamal Love <rosekamallove@gmail.com>
2022-04-29 23:10:21 +0530

    color changing favicon

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,12 +1,14 @@
-import { Box, Container } from '@chakra-ui/react'
+import { Box, Container, useColorModeValue } from '@chakra-ui/react'
 import Head from 'next/head'
 import Navbar from '../navbar.js'
 
 const Main = ({ children, router }) => {
+  const logoImg = `/images/logo${useColorModeValue('-light', '-dark')}.png`
   return (
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width initial-scale=1" />
+        <link rel="shortcut icon" href={logoImg} />
         <title>Rose Kamal Love</title>
       </Head>
 
