a40a42d9d9d37ae64468e28af6d141cde025dc79
Rose Kamal Love <rosekamallove@gmail.com>
2022-04-29 23:00:25 +0530

    new logo

--- components/logo.js
+++ components/logo.js
@@ -1,30 +1,27 @@
-import Link from 'next/link'
-import Image from 'next/image'
 import { Text, useColorModeValue } from '@chakra-ui/react'
 import styled from '@emotion/styled'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const LogoBox = styled.span`
   font-weight: bold;
   font-size: 18px;
   display: inline-flex;
   align-items: center;
-  height: 30px;
+  margin-top: 5px;
+  height: 50px;
   line-height: 20px;
   padding: 10px;
   transition: ease 0.2s;
-
-  &:hover img {
-    transform: rotate(20deg);
-  }
 `
 
 const Logo = () => {
-  const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
+  const logoImg = `/images/logo${useColorModeValue('-light', '-dark')}.png`
   return (
     <Link href="/">
       <a>
         <LogoBox>
-          <Image src={footPrintImg} width={20} height={20} alt="logo" />
+          <Image src={logoImg} width={40} height={40} alt="logo" />
           <Text
             color={useColorModeValue('gray.800', 'whiteAlpha.900')}
             fontFamily="M PLUS Rounded 1c"
