10fddd1acde585ba333470b4030f1933f12d3c7f
Rose Kamal Love <rosekamallove@gmail.com>
2022-04-30 20:09:34 +0530

    adding prism highlighting went horribley

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion'
 import Head from 'next/head'
+import { useEffect } from 'react'
 import { GridItemStyle } from '../grid-item'
 
 const variants = {
@@ -8,28 +9,34 @@ const variants = {
   exit: { opacity: 0, x: -0, y: 20 }
 }
 
-const Layout = ({ children, title }) => (
-  <motion.article
-    initial="hidden"
-    animate="enter"
-    exit="exit"
-    variants={variants}
-    transition={{ duration: 0.2, type: 'easeInOut' }}
-    style={{ position: 'relative' }}
-  >
-    <>
-      {title && (
-        <Head>
-          <title>{title} - Rose Kamal Love</title>
-          <meta name="twitter:title" content={title} />
-          <meta property="og:title" content={title} />
-        </Head>
-      )}
-      {children}
+const Layout = ({ children, title }) => {
+  useEffect(() => {
+    Prism.highlightAll()
+  }, [])
 
-      <GridItemStyle />
-    </>
-  </motion.article>
-)
+  return (
+    <motion.article
+      initial="hidden"
+      animate="enter"
+      exit="exit"
+      variants={variants}
+      transition={{ duration: 0.2, type: 'easeInOut' }}
+      style={{ position: 'relative' }}
+    >
+      <>
+        {title && (
+          <Head>
+            <title>{title} - Rose Kamal Love</title>
+            <meta name="twitter:title" content={title} />
+            <meta property="og:title" content={title} />
+          </Head>
+        )}
+        {children}
+
+        <GridItemStyle />
+      </>
+    </motion.article>
+  )
+}
 
 export default Layout
