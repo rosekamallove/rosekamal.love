c84f379e274587a8ac94b548f9b3e4a30ec33517
Rose Kamal Love <rosekamallove@gmail.com>
2022-05-07 01:58:36 +0530

    new colors

--- components/navbar.js
+++ components/navbar.js
@@ -27,7 +27,7 @@ const LinkItem = ({ href, path, _target, children, ...props }) => {
     <NextLink href={href} passHref>
       <Link
         p={2}
-        bg={active ? 'grassTeal' : undefined}
+        bg={active ? 'teal.500' : undefined}
         color={active ? '#202023' : inactiveColor}
         _target={_target}
         {...props}
