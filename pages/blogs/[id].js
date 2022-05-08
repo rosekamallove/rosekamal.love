3ce7eafb7a93f375e1536c50bb504aec0e630a5b
Rose Kamal Love <rosekamallove@gmail.com>
2022-05-08 12:23:01 +0530

    onClose(); gp

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -102,8 +102,8 @@ export default function Post({ postData, id }) {
         setEmail('')
         setMessage('')
       }
+      onClose()
     })
-    submitted ? onClose() : ''
   }
 
   return (
