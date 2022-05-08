e5d97f567f810838e06365649e858154d3add53d
Rose Kamal Love <rosekamallove@gmail.com>
2022-05-08 11:17:29 +0530

    implemented: api/contact using nodemailer

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,3 +1,31 @@
 export default function handler(req, res) {
-  console.log(req.body)
+  require('dotenv').config()
+
+  const nodemailer = require('nodemailer')
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: 'smtp.gmail.com',
+    service: 'gmail',
+    auth: {
+      type: 'login',
+      user: 'private.rosekamallove@gmail.com',
+      pass: process.env.password
+    },
+    secure: true
+  })
+
+  const mailData = {
+    from: 'private.roseakamallove@gmail.com',
+    to: 'rosekamallove@gmail.com',
+    subject: `Message From ${req.body.name}`,
+    text: req.body.message + ' | Sent from: ' + req.body.email,
+    html: `<div>${req.body.message}</div><p>Sent from:
+    ${req.body.email}</p>`
+  }
+
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) console.log(err)
+    else console.log(info)
+  })
+  res.send(200).end()
 }
