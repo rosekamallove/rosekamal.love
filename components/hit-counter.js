fd0359de3ab304d1cfbc38039cc426289521cafd
Rose Kamal Love <rosekamallove@gmail.com>
2022-05-09 20:44:01 +0530

    fixed: !update hits in production

--- components/hit-counter.js
+++ components/hit-counter.js
@@ -6,9 +6,9 @@ export default function HitCounter({ slug }) {
   const [hits, setHits] = React.useState(undefined)
   React.useEffect(() => {
     // Don't count hits on localhost
-    // if (process.env.NODE_ENV !== 'production') {
-    //   return
-    // }
+    if (process.env.NODE_ENV !== 'production') {
+      return
+    }
     // Invoke the function by making a request.
     // Update the URL to match the format of your platform.
     fetch(`/api/register-hit?slug=${slug}`, {
