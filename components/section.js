e9bac3292f585f7ea404afb14f8caebfd7b2940e
Rose Kamal Love <rosekamallove@gmail.com>
2022-05-10 09:30:39 +0530

    faster: animations

--- components/section.js
+++ components/section.js
@@ -11,7 +11,7 @@ const Section = ({ children, delay = 0 }) => (
   <StyledDiv
     initial={{ y: 10, opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
-    transition={{ duration: 0.2, delay }}
+    transition={{ duration: 0.1, delay }}
     mb={6}
   >
     {children}
