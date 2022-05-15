d7dd45e56446517839afd511a1a9f68c0d24e263
Rose Kamal Love <rosekamallove@gmail.com>
2022-05-15 12:15:36 +0530

    implemented: searchBar(title, description)

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ import { IconLinks } from './icon-links'
 const Footer = () => {
   return (
     <>
-      <IconLinks />
+      <IconLinks size="md" />
       <Box align="center" fontSize="xs" opacity={0.5}>
         Copyright &copy; {new Date().getFullYear()} • Rose Kamal Love
       </Box>
--- components/render-blog-list.js
+++ components/render-blog-list.js
@@ -1,13 +1,48 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { List } from '@chakra-ui/react'
+import { List, useToast } from '@chakra-ui/react'
 import NextLink from 'next/link'
+import { useState } from 'react'
 import { BlogListItem } from './blog-list-item'
+import { SearchBar } from './search-bar'
 
 export const RenderBlogs = ({ allPostsData, renderDescription, count }) => {
+  const toast = useToast()
+  const id = 'test-toast'
+
   count = count == -1 ? allPostsData.length : count
+  const [postData, setPostData] = useState(allPostsData)
+  const [searchField, setSearchField] = useState('')
+
+  const handleChange = e => {
+    e.preventDefault()
+    setSearchField(e.target.value)
+    if (e.target.value === '') return setPostData(allPostsData)
+
+    const filteredPosts = postData.filter(
+      post =>
+        post.title.toLowerCase().includes(searchField.toLowerCase()) ||
+        post.og_description.toLowerCase().includes(searchField.toLowerCase())
+    )
+    setPostData(filteredPosts)
+
+    if (filteredPosts.length === 0 && !toast.isActive(id))
+      toast({
+        description: 'No posts found',
+        status: 'error',
+        position: 'top-right',
+        isClosable: true,
+        id
+      })
+  }
+
   return (
     <List>
-      {allPostsData.map(
+      <SearchBar
+        renderDescription={renderDescription}
+        handleChange={handleChange}
+        searchField={searchField}
+      />
+      {postData.map(
         (
           {
             id,
