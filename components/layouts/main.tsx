6472ef3e9abe54b1a1303fcdcc8826ec16bb6860
Rose Kamal Love <rosekamallove@gmail.com>
2022-05-18 00:45:20 +0530

    migrated: pages/** && components/layouts/**

--- components/layouts/main.tsx
+++ components/layouts/main.tsx
@@ -1,8 +1,14 @@
 import { Box, Container, useColorModeValue } from '@chakra-ui/react'
+import { AppProps } from 'next/app.js'
 import Head from 'next/head'
+import React from 'react'
 import Navbar from '../navbar.js'
 
-const Main = ({ children, router }) => {
+interface Props extends AppProps {
+  children: React.ReactNode
+}
+
+const Main: React.FC<Props> = ({ children, router }) => {
   const logoImg = `/images/logo${useColorModeValue('-light', '-dark')}.png`
   return (
     <Box as="main" pb={8}>
@@ -11,9 +17,7 @@ const Main = ({ children, router }) => {
         <link rel="shortcut icon" href={logoImg} />
         <title>Rose Kamal Love</title>
       </Head>
-
       <Navbar path={router.asPath} />
-
       <Container maxW="container.md" pt={14}>
         {children}
       </Container>
