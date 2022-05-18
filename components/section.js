4b2869e4eb30b90b3e0968dfb4968f377a1f75fb
Rose Kamal Love <rosekamallove@gmail.com>
2022-05-18 10:57:01 +0530

    fixed: toString issue;

--- components/section.js
+++ components/section.js
@@ -11,7 +11,7 @@ const Section = ({ children, delay = 0 }) => (
   <StyledDiv
     initial={{ y: 10, opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
-    transition={{ duration: 0.1, delay: delay.toString }}
+    transition={{ duration: 0.1, delay: delay }}
     mb={6}
   >
     {children}
