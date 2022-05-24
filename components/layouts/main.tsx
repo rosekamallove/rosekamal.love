7d3c7f6bcb7833f68fb75645bddf682b55be8e9b
Rose Kamal Love <rosekamallove@gmail.com>
2022-05-24 12:53:40 +0530

    fixed: seo

--- components/layouts/main.tsx
+++ components/layouts/main.tsx
@@ -15,7 +15,24 @@ const Main: React.FC<Props> = ({ children, router }) => {
       <Head>
         <meta name="viewport" content="width=device-width initial-scale=1" />
         <link rel="shortcut icon" href={logoImg} />
-        <title>Rose Kamal Love</title>
+        <meta
+          name="description"
+          content="Rose Kamal Love's Personal Blog / Portfolio"
+        />
+        <meta name="author" content="Rose Kamal Love" />
+        <meta name="author" content="rosek" />
+        <meta name="twitter:title" content="Rose Kamal Love" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@RoseKamalLove1" />
+        <meta name="twitter:creator" content="@RoseKamalLove1" />
+        <meta
+          name="twitter:image"
+          content="https://rosekamallove.vercel.app/rosek.jpg"
+        />
+        <meta property="og:site_name" content="Rose Kamal Love" />
+        <meta name="og:title" content="Rose Kamal Love" />
+        <meta property="og:type" content="website" />
+        <title>Rose Kamal Love - Blog</title>
       </Head>
       <Navbar path={router.asPath} />
       <Container maxW="container.md" pt={14}>
