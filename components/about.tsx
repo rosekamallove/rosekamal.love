c202c55835a8940816761a9f6779e95188f5881d
Rose Kamal Love <rosekamallove@gmail.com>
2022-05-26 18:56:39 +0530

    fix: grammerly

--- components/about.tsx
+++ components/about.tsx
@@ -22,10 +22,10 @@ export const About: React.FC = () => {
           I like solving problems, algorithmic as well as the ones I face when
           developing cool projects like{' '}
           <A href="https://youtemy.tech">YouTemy</A>. I love music, and am
-          something of a &quot;HTML Coder&quot; in the world of music production
-          too 😅 checkout my
+          something of an &quot;HTML Coder&quot; in the world of music
+          production too 😅 check out my
           <A href="https://soundcloud.com/user-276723683"> SoundCloud</A>. I
-          also take awesome pictures as a hobby, checkout my{' '}
+          also take awesome pictures as a hobby, check out my{' '}
           <A href="https://instagram.com/rosekamallove">Instagram</A>.
         </Text>
         <Text>
@@ -33,14 +33,14 @@ export const About: React.FC = () => {
           <A href="https://trysolvio.ai/">Solvio</A> /{' '}
           <A href="https://trysolvio.ai/">BlendEd</A> as a Front-End Intern, and
           will help <A href="https://librehealth.io/">LibreHealth</A> build
-          Libre Medical Record Systems this summer as an Open Source Contribter
+          Libre Medical Record Systems this summer as an Open Source Contributor
           under the{' '}
           <A href="summerofcode.with.google.com">Google Summer of Code </A>
           Program ✨
         </Text>
         <Text>
           I write blog posts about <em>Internships</em>,{' '}
-          <em>Web Development</em>, <em>Open Source</em>,<em>Productivity</em>{' '}
+          <em>Web Development</em>, <em>Open Source</em>,<em> Productivity,</em>{' '}
           and more. <strong>Happy Reading </strong>☕
         </Text>
         <Flex my={4}>
