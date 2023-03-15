8d3209b6bdb9a2a949e248dd7be1a218e2a59fd0
rosekamallove <rosekamallove@gmail.com>
2023-03-15 14:57:56 +0530

    updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,6 +24,7 @@ export default function Home({
 }) {
   const hello = api.example.hello.useQuery({ text: "Hello from TRPC" });
   console.log(hello.data?.greeting);
+
   return (
     <Layout home>
       <Head>
